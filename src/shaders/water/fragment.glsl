033d1c417e80fb1eee66c128665e23badc7485ae
Josiah Webb <jj.webb520@gmail.com>
2022-11-30 16:07:23 -0700

    renames elevationMultiplier to mixStrength

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -6,8 +6,8 @@ uniform float uColorMultiplier;
 varying float vElevation;
 
 void main() {
-    float elevationMultiplier = (vElevation + uColorOffset) * uColorMultiplier;
-    vec3 color = mix(uDepthColor, uSurfaceColor, elevationMultiplier);
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
     
     gl_FragColor = vec4(color, 1.0);
 }
